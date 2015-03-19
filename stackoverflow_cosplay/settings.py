0e2d86a155e5e0fa601dea13785facae64ce374c
Sashkow <lykhenko.olexandr@gmail.com>
2015-03-19 11:21:42 +0200

    modiffied settings, db

--- stackoverflow_cosplay/settings.py
+++ stackoverflow_cosplay/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+print '-->', BASE_DIR
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
@@ -104,9 +105,9 @@ DATABASES = {
     }
 }
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+# # Parse database configuration from $DATABASE_URL
+# import dj_database_url
+# DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
