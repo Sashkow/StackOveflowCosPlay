e44a773ba96efd841b694557f70f6f59c8bcc263
Sashkow <lykhenko.olexandr@gmail.com>
2015-03-19 11:39:20 +0200

    modiffied plan, added serializer to settings

--- stackoverflow_cosplay/settings.py
+++ stackoverflow_cosplay/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-print '-->', BASE_DIR
+
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
@@ -21,6 +21,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
